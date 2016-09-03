f7f0f1d948a125ad4b98db6bb868c278b5d5f7a4
mohammad2016mr <mohammad.kingme@gmail.com>
2016-09-03 01:32:48 -0800

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -208,7 +208,7 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 
 	if matches[1] == 'help' and msg.to.type == 'user' or matches[1] == 'pmhelp' and is_admin1(msg) and msg.to.type ~= 'user' then
       	savelog(msg.to.id, name_log.." ["..msg.from.id.."] used pm help")
-		text = "Welcome force Bot \n admin force Bot : \n @Vandevoorde \n channel force Bot : \n @forcetg_ch \n send me \n /superhelp \n /chats \n /force "
+		text = "Welcome force Bot \n admin force Bot : \n @gpsudo \n channel force Bot : \n @forcetg_ch \n send me \n /superhelp \n /chats \n /force "
      	return text
     end
 
